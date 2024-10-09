2a81e0c335c8a9f9bc033dc1643206349795c613
wottan32 <mariotorreslagos@gmail.com>
2024-10-09 12:05:35 -0300

    Your commit message here

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import Message from "./Message";
+import ListGroup from "./components/ListGroup";
+
+function App() {
+  let items = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
+
+  const handleSelectItem = (item: string) => {
+    console.log(item);
+  };
+
+  return (
+    <div>
+      <ListGroup items={items} heading="Cities" onSelectItem={handleSelectItem} />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Message.tsx
@@ -0,0 +1,10 @@
+// PascalCasing
+function Message() {
+    //JSX JavaScript XML
+    const name = 'Mario';
+    if (name)
+        return <h1>Hello {name}</h1>;
+    return <h1>Hello Stranger</h1>;
+}
+
+export default Message;
